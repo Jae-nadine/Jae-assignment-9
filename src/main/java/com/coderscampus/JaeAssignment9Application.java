44c2cb6272f9c5262b15bd2ec797cce62d5b0d3d
Jae-Nadine <jae.nadine@icloud.com>
2024-12-12 17:13:54 -0700

    Initial Commit
    Making sure everything is synced from Eclipse to Github Desktop

---
+++ src/main/java/com/coderscampus/JaeAssignment9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaeAssignment9Application.class, args);
+	}
+
+}
